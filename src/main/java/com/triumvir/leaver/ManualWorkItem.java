e75fe24d7c51a6137cec1957d1b956739757bf37
victor1511 <vcenteno@triumvircorp.com>
2016-07-27 14:11:34 -0600

    Create a manual Work Item with the applications that doesn't has provisioning support

--- src/main/java/com/triumvir/leaver/ManualWorkItem.java
+++ src/main/java/com/triumvir/leaver/ManualWorkItem.java
@@ -7,14 +7,20 @@ import org.apache.log4j.Logger;
 
 import sailpoint.api.SailPointContext;
 import sailpoint.object.Application;
+import sailpoint.object.ApprovalItem;
+import sailpoint.object.ApprovalSet;
+import sailpoint.object.Attributes;
 import sailpoint.object.Identity;
 import sailpoint.object.Link;
+import sailpoint.object.WorkItem;
+import sailpoint.object.WorkItem.Level;
+import sailpoint.persistence.Sequencer;
 import sailpoint.tools.GeneralException;
 
 public class ManualWorkItem {
 	final static Logger logger = Logger.getLogger(ManualWorkItem.class);
 	
-	public List<String> getApplicationsWithProvSupport(Identity identity, SailPointContext context) throws GeneralException
+	private List<String> getApplicationsWithNoProvSupport(Identity identity, SailPointContext context) throws GeneralException
 	{
 		List<Link> accounts = new ArrayList<Link>();
 		List<String> appList = new ArrayList<String>();
@@ -44,14 +50,52 @@ public class ManualWorkItem {
 		{
 			Application app = new Application();
 			app = context.getContext().getObjectByName(Application.class, application);
-			if(!app.isSupportsProvisioning())
+			if(app.isSupportsProvisioning())
 			{
-				logger.debug("This app doesn't support provisioning " + application);
 				temporalList.add(application);
 			}
 		}
-		applicationList.removeAll(temporalList);
-		
+		applicationList.removeAll(temporalList);		
 		return applicationList;
+	}
+	
+	public WorkItem createWorkItem(Identity identity, SailPointContext context, Identity launcher) throws GeneralException 
+	{
+		
+		WorkItem  workItem = new WorkItem();
+		Sequencer sequencer = new Sequencer(); // Get the request number for the WorkItem.
+		
+		workItem.setType(WorkItem.Type.ManualAction);
+		workItem.setName(sequencer.generateId(context.getContext(), workItem));
+		workItem.setTarget(identity);
+		workItem.setTargetClass(Identity.class.getName());
+		workItem.setRequester(launcher);
+		workItem.setLevel(Level.High);
+		workItem.setOwner(identity.getManager());
+		workItem.setTargetClass(sailpoint.object.Identity.class);
+		workItem.setHandler(sailpoint.api.Workflower.class);
+		workItem.setRenderer("lcmManualActionsRenderer.xhtml");
+		workItem.setDescription("Manual actions for the applications below");
+		
+		Attributes<String, Object> wItemAttributes = new Attributes<String, Object>();
+		ApprovalSet approvalSet = new ApprovalSet();
+		
+		
+		List <String> appsWithNoProv = getApplicationsWithNoProvSupport(identity, context);
+		for(String app : appsWithNoProv)
+		{
+			ApprovalItem approvalItem = new ApprovalItem();
+			Application newApp = context.getObjectByName(Application.class, app);
+			approvalItem.setApplication(newApp);
+			approvalItem.setNativeIdentity(identity.getName());
+			approvalItem.setOperation("Delete"); 
+			approvalItem.setValue(newApp);
+			approvalSet.add(approvalItem);
+		}
+		
+		wItemAttributes.put("approvalSet", approvalSet);
+		workItem.setAttributes(wItemAttributes);
+	
+		return workItem;
 	}
 }
